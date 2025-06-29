e630f918c52fbfdb814bc07329e39343e8852805
Viviane <viviane.sp13@gmail.com>
2025-06-29 16:35:30 -0300

    feat: add repositories emprestimo livro usuarios

--- GerenciadorBiblioteca.Infra/Repositories/UsuarioRepository.cs
+++ GerenciadorBiblioteca.Infra/Repositories/UsuarioRepository.cs
@@ -1,6 +1,34 @@
-﻿namespace GerenciadorBiblioteca.Infra.Repositories
+﻿using GerenciadorBiblioteca.Domain.Entities;
+using GerenciadorBiblioteca.Domain.Interfaces;
+
+namespace GerenciadorBiblioteca.Infra.Repositories
 {
-    public class UsuarioRepository
+    public class UsuarioRepository : IUsuarioRepository
     {
+        private static readonly List<Usuario> _usuarios = new();
+
+        public void Adicionar(Usuario usuario)
+        {
+            _usuarios.Add(usuario);
+        }
+
+        public Usuario? ObterPorId(Guid id)
+        {
+            return _usuarios.FirstOrDefault(u => u.Id == id);
+        }
+
+        public IEnumerable<Usuario> ListarTodos()
+        {
+            return _usuarios;
+        }
+
+        public bool Remover(Guid id)
+        {
+            var usuario = ObterPorId(id);
+            if (usuario == null) return false;
+
+            _usuarios.Remove(usuario);
+            return true;
+        }
     }
 }
