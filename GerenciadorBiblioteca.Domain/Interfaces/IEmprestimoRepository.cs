a4d68d4b03ff420fff9824b606483de81f5a7414
Viviane <viviane.sp13@gmail.com>
2025-06-29 14:44:19 -0300

    feat: interfaces dos repositorios

--- GerenciadorBiblioteca.Domain/Interfaces/IEmprestimoRepository.cs
+++ GerenciadorBiblioteca.Domain/Interfaces/IEmprestimoRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace GerenciadorBiblioteca.Domain.Interfaces
+﻿using GerenciadorBiblioteca.Domain.Entities;
+
+namespace GerenciadorBiblioteca.Domain.Interfaces
 {
     public interface IEmprestimoRepository
     {
+        void Adicionar(Emprestimo emprestimo);
+        Emprestimo? ObterPorId(Guid id);
+        IEnumerable<Emprestimo> ListarTodos();
+        void Atualizar(Emprestimo emprestimo);
     }
 }
