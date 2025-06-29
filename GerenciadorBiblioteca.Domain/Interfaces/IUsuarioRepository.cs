255cc21aab66911ffc2f9d095efb9fb1904fad23
Viviane <viviane.sp13@gmail.com>
2025-06-29 13:29:38 -0300

    feat: add IUsuarioRepository

--- GerenciadorBiblioteca.Domain/Interfaces/IUsuarioRepository.cs
+++ GerenciadorBiblioteca.Domain/Interfaces/IUsuarioRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace GerenciadorBiblioteca.Domain.Interfaces
+﻿using GerenciadorBiblioteca.Domain.Entities;
+
+namespace GerenciadorBiblioteca.Domain.Interfaces
 {
     public interface IUsuarioRepository
     {
+        void Adicionar(Usuario usuario);
+        Usuario? ObterPorId(Guid id);
+        IEnumerable<Usuario> ListarTodos();
+        bool Remover(Guid id);
     }
 }
